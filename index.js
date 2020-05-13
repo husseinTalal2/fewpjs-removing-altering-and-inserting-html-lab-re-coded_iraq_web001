6e250139bd70629afbf8d3db1e5ad6873c54f5e7
Hussein Talal <hossientalal2@gmail.com>
2020-05-13 21:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "is the champion";
+const main =document.querySelector('main').remove();
+main.remove();
